01a16eac94733f17360e63c22ef59eac710237d2
Cynthia Freund <cfreund@creditacceptance.com>
2024-09-24 13:15:28 -0400

    adding react puppy bowl files to repository

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/Home"; 
+import SinglePlayer from "./pages/SinglePlayer";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header/Header";
+import CreatePlayer from "./pages/CreatePlayer";
+
+import{Routes, Route} from "react-router-dom";
+
+function App() {
+ 
+  return (
+    <>
+    <Header/>
+    <Routes>
+      <Route path = "/" element={<Home/>}/>
+      <Route path = "/details/:id" element={<SinglePlayer/>}/>
+      <Route path="/create" element={<CreatePlayer/>}/>
+      <Route path = "*" element={<NotFound/>}/>
+    </Routes>
+   </> 
+  );
+}
+
+export default App
---
+++ src/components/PlayerCardList/PlayerCardList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PlayerCard from "../PlayerCard/PlayerCard";
+import "./PlayerCardList.css"
+
+function PlayerCardList({players}) {
+    return (
+    <div className="player-card-list">
+        {players.map((player) =>(
+            <PlayerCard player={player} key= {player?.id}/>
+        ))}
+    </div>
+    );
+}
+export default PlayerCardList;
